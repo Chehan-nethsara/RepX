c0d0a56d14f56fcb7bc3befd1d228fee17168b20
Chehan-nethsara <chehan4@gmail.com>
2020-03-19 14:24:59 +0530

    I commit to RepX these Ui Home Ui, Dealer Profile and Add dealer UI.

---
+++ app/src/main/java/com/example/repx/dealer.java
@@ -0,0 +1,17 @@
+package com.example.repx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+
+public class dealer extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dealer);
+    }
+}
