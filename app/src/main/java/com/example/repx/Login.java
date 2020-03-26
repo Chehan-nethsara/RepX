d8cf389be41c659bcb0cf9d3d300cfe0149d86c2
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-03-27 00:32:28 +0530

    adding cards to the design and connecting interfaces

--- app/src/main/java/com/example/repx/Login.java
+++ app/src/main/java/com/example/repx/Login.java
@@ -2,13 +2,24 @@ package com.example.repx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button SigninButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SigninButton = findViewById(R.id.Sign_up);
+        SigninButton.setOnClickListener((new View.OnClickListener() {
+            public void onClick(View view) {
+               startActivity(new Intent(Login.this, Signup.class ));
+            }
+        }));
+
     }
 }
